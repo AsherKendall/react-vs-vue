3916f82bb33812aae8c97e7571639452278358a6
Asher Griess <36317228+AsherKendall@users.noreply.github.com>
2025-10-27 15:53:27 -0500

    change vite stuff

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url';
-import path, {dirname} from 'path'
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +7,7 @@ export default defineConfig({
   build: {
     outDir: '../dist/react-app',
     lib: {
-      entry: path.resolve(__dirname, 'src/main.jsx'),
+      entry: './src/main.jsx',
       name: 'ReactApp',
       formats: ['es'],
       fileName: 'react-entry'
