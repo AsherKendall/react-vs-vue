a1df9eba5082116c2e8559792a5dbec7f34e3360
Asher Griess <36317228+AsherKendall@users.noreply.github.com>
2025-11-06 12:49:19 -0600

    testing bibtex code

--- vue-app/src/components/templates/GenericPage.vue
+++ vue-app/src/components/templates/GenericPage.vue
@@ -3,16 +3,43 @@
     <h1 class="green">{{ title }}</h1>
   </div>
   <slot></slot>
+  <div v-html="bibliography"></div>
 </template>
 
 <script>
+import { Cite, plugins } from '@citation-js/core'
+import '@citation-js/plugin-bibtex'
+
+plugins.add('@citation-js/plugin-bibtex')
 export default {
+  data() {
+    return {
+      bibliography: '',
+    }
+  },
   name: 'BasicPage', // Optional but recommended for debugging
   props: {
     title: {
       type: String,
       default: 'Default Title',
     },
+  },
+  async mounted() {
+    try {
+      const response = await fetch('/react-vs-vue/#/refs.bib')
+      const bibtexContent = await response.text()
+      console.log(bibtexContent)
+
+      const cite = new Cite(bibtexContent)
+      this.bibliography = cite.format('bibliography', {
+        format: 'html',
+        template: 'ieee', // Or 'vancouver', 'ieee', etc.
+        lang: 'en-US',
+      })
+    } catch (error) {
+      console.error('Error loading or parsing BibTeX:', error)
+      console.error(error.message)
+    }
   },
 }
 </script>
